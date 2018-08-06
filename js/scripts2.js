c41ad73c0d7e42030e2844c8f25158183efe91aa
Julius Bade and Mark Mangahas <julius@>
2018-08-06 13:13:22 -0700

    added four math operations

--- js/scripts2.js
+++ js/scripts2.js
@@ -25,5 +25,5 @@ var divide= function(number1, number2) {
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
+var result = divide(number1, number2);
 alert(result);
