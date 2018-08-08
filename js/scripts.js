8e2cc11a3562a6854404c26230aa0604952e7ede
Julius Bade and Mark Mangahas <julius@>
2018-08-08 15:12:27 -0700

    finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,43 @@
 // Business (or back-end) logic:
 
-var add = function(number1, number2) {
-  return number1 + number2;
+var add = function(num1, num2) {
+  return num1 + num2;
 };
 
-var subtract = function(number1, number2) {
-  return number1 - number2;
+var subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-var multiply = function(number1, number2) {
-  return number1 * number2;
+var multiply = function(num1, num2) {
+  return num1 * num2;
 };
 
-var divide = function(number1, number2) {
-  return number1 / number2;
+var divide = function(num1, num2) {
+  return num1 / num2;
 };
 
 // User interface (or front-end) logic:
-
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-   event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
+$("#check button").click(function() {
+  event.preventDefault();
+  var num1 = parseInt($("#num1").val());
+  var num2 = parseInt($("#num2").val());
+  if($(this).attr("id")=="add") {
+    var result = add(num1, num2);
     $("#output").text(result);
-  });
+    $("#check")[0].reset();
+  } else if($(this).attr("id")=="subtract") {
+    var result = subtract(num1, num2);
+    $("#output").text(result);
+    $("#check")[0].reset();
+  } else if($(this).attr("id")=="divide") {
+    var result = divide(num1, num2);
+    $("#output").text(result);
+    $("#check")[0].reset();
+  } else if ($(this).attr("id")=="multiply") {
+    var result = multiply(num1, num2);
+    $("#output").text(result);
+    $("#check")[0].reset();
+  }
+ });
 });
