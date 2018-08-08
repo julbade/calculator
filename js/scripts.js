a0a9b3b53d3d10ac99a680aaca9c81e861432447
Julius Bade and Mark Mangahas <mark@>
2018-08-08 13:40:53 -0700

    added user interface in calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
-var bmi = function(weight, height) {
-  return weight / (height*height)
+// Business (or back-end) logic:
 
- };
+var add = function(number1, number2) {
+  return number1 + number2;
+};
 
-var weight = parseFloat(prompt("Enter weight in kg:"));
-var height = parseFloat(prompt("Enter height in meters:"));
-var result = bmi(weight,height)
-alert(result)
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// User interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+   event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
